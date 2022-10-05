4ff8e80af0c1b6f9cf5ed10a8b5eace1aae909f1
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-10-05 20:43:46 +0000

    CLI version working

--- js/sandbox.js
+++ js/sandbox.js
@@ -8,7 +8,10 @@ let gameState = {
     playerSymbol1: "X",
     playerName2: "O",
     playerSymbol2: "O",
-    gameBoard: ["", "", "", "", "", "", "", "", ""]
+    gameBoard: ["", "", "", "", "", "", "", "", ""],
+    turn: 1,
+    gameOver: false,
+    whoseTurn: 1
 };
 
 function updateState() {
@@ -47,32 +50,40 @@ function checkForWin() {
 let b = gameState.gameBoard;
     if ((
         (b[0] === b[1]) &&
-        (b[1] === b[2])
+        (b[1] === b[2]) &&
+        (b[0] !== "")
     ) || (
         (b[3] === b[4]) &&
-        (b[4] === b[5])
+        (b[4] === b[5]) &&
+        (b[3] !== "")
     ) || (
         (b[6] === b[7]) &&
-        (b[7] === b[8])
+        (b[7] === b[8]) &&
+        (b[6] !== "")
     ) || (
         (b[0] === b[3]) &&
-        (b[3] === b[6])
+        (b[3] === b[6]) &&
+        (b[0] !== "")
     ) || (
         (b[1] === b[4]) &&
-        (b[4] === b[7])
+        (b[4] === b[7]) &&
+        (b[1] !== "")
     ) || (
         (b[2] === b[5]) &&
-        (b[5] === b[8])
+        (b[5] === b[8]) &&
+        (b[2] !== "")
     ) || (
         (b[0] === b[4]) &&
-        (b[4] === b[8])
+        (b[4] === b[8]) &&
+        (b[0] !== "")
     ) || (
         (b[2] === b[4]) &&
-        (b[4] === b[6])
+        (b[4] === b[6]) &&
+        (b[2] !== "")
     )) {
         console.log("Game Over!");
-    }
-        
+        gameState.gameOver = true;
+    }        
 };
 
 console.log("Player Name 1: ", gameState.playerName1);
@@ -83,156 +94,153 @@ console.log("Start game.");
 
 renderBoard();
 
-console.log("Turn number 1. ", gameState.playerName1, "'s turn.");
-let square = prompt("Player 1: Choose a square, 1-9.");
-if (checkSquare(square-1)) {
-    console.log("Valid square selection.");
-    console.log(gameState.playerName1, " plays square number ", square);
-    updateBoard(gameState.playerName1, square-1);
-    console.log("Updated game board, turn number 2:");
-    renderBoard();
-} else {
-    console.log("Invalid selection.");
-};
+while (gameState.gameOver !== true) {
+    let playerName = "";
+    let playerSymbol = "";
+    console.log("Turn number ", gameState.turn);
+    if (gameState.whoseTurn == 1) {
+        playerName = gameState.playerName1;
+        playerSymbol = gameState.playerSymbol1;
+    } else {
+        playerName = gameState.playerName2;
+        playerSymbol = gameState.playerSymbol2;
+    }
+    console.log(`It is ${playerName}'s turn.`);
+    let choice = prompt(`${playerName}, choose a game square 1-9.`);
+    if (checkSquare(choice-1)) {
+        console.log("Valid square selection.");
+        console.log(playerName, " plays square number ", choice);
+        updateBoard(playerSymbol, choice-1);
+        console.log("Updated game board:");
+        renderBoard();
+    } else {
+        console.log("Invalid selection.");
+    };
+    checkForWin();
+    gameState.turn++;
+    if (gameState.whoseTurn == 1) {
+        gameState.whoseTurn = 2;
+    } else {
+        gameState.whoseTurn = 1;
+    }
+}
 
-console.log("Turn number 2. ", gameState.playerName2, "'s turn.");
-square = prompt("Player 2: Choose a square, 1-9.");
-if (checkSquare(square-1)) {
-    console.log("Valid square selection.");
-    console.log(gameState.playerName2, " plays square number ", square);
-    updateBoard(gameState.playerName2, square-1);
-    console.log("Updated game board, turn number 3:");
-    renderBoard();
-} else {
-    console.log("Invalid selection.");
-};
 
-console.log("Turn number 3. ", gameState.playerName1, "'s turn.");
-square = prompt("Player 1: Choose a square, 1-9.");
-if (checkSquare(square-1)) {
-    console.log("Valid square selection.");
-    console.log(gameState.playerName1, " plays square number ", square);
-    updateBoard(gameState.playerName1, square-1);
-    console.log("Updated game board, turn number 4:");
-    renderBoard();
-} else {
-    console.log("Invalid selection.");
-};
+// console.log("Turn number 1. ", gameState.playerName1, "'s turn.");
+// let square = prompt("Player 1: Choose a square, 1-9.");
+// if (checkSquare(square-1)) {
+//     console.log("Valid square selection.");
+//     console.log(gameState.playerName1, " plays square number ", square);
+//     updateBoard(gameState.playerName1, square-1);
+//     console.log("Updated game board, turn number 2:");
+//     renderBoard();
+// } else {
+//     console.log("Invalid selection.");
+// };
 
-console.log("Turn number 4. ", gameState.playerName2, "'s turn.");
-square = prompt("Player 2: Choose a square, 1-9.");
-if (checkSquare(square-1)) {
-    console.log("Valid square selection.");
-    console.log(gameState.playerName2, " plays square number ", square);
-    updateBoard(gameState.playerName2, square-1);
-    console.log("Updated game board, turn number 5:");
-    renderBoard();
-} else {
-    console.log("Invalid selection.");
-};
+// console.log("Turn number 2. ", gameState.playerName2, "'s turn.");
+// square = prompt("Player 2: Choose a square, 1-9.");
+// if (checkSquare(square-1)) {
+//     console.log("Valid square selection.");
+//     console.log(gameState.playerName2, " plays square number ", square);
+//     updateBoard(gameState.playerName2, square-1);
+//     console.log("Updated game board, turn number 3:");
+//     renderBoard();
+// } else {
+//     console.log("Invalid selection.");
+// };
 
-console.log("Turn number 5. ", gameState.playerName2, "'s turn.");
-square = prompt("Player 1: Choose a square, 1-9.");
-if (checkSquare(square-1)) {
-    console.log("Valid square selection.");
-    console.log(gameState.playerName1, " plays square number ", square);
-    updateBoard(gameState.playerName1, square-1);
-    console.log("Updated game board, turn number 6:");
-    renderBoard();
-} else {
-    console.log("Invalid selection.");
-};
+// console.log("Turn number 3. ", gameState.playerName1, "'s turn.");
+// square = prompt("Player 1: Choose a square, 1-9.");
+// if (checkSquare(square-1)) {
+//     console.log("Valid square selection.");
+//     console.log(gameState.playerName1, " plays square number ", square);
+//     updateBoard(gameState.playerName1, square-1);
+//     console.log("Updated game board, turn number 4:");
+//     renderBoard();
+// } else {
+//     console.log("Invalid selection.");
+// };
 
-checkForWin();
+// console.log("Turn number 4. ", gameState.playerName2, "'s turn.");
+// square = prompt("Player 2: Choose a square, 1-9.");
+// if (checkSquare(square-1)) {
+//     console.log("Valid square selection.");
+//     console.log(gameState.playerName2, " plays square number ", square);
+//     updateBoard(gameState.playerName2, square-1);
+//     console.log("Updated game board, turn number 5:");
+//     renderBoard();
+// } else {
+//     console.log("Invalid selection.");
+// };
 
-console.log("Turn number 6. ", gameState.playerName2, "'s turn.");
-square = prompt("Player 2: Choose a square, 1-9.");
-if (checkSquare(square-1)) {
-    console.log("Valid square selection.");
-    console.log(gameState.playerName2, " plays square number ", square);
-    updateBoard(gameState.playerName2, square-1);
-    console.log("Updated game board, turn number 7:");
-    renderBoard();
-} else {
-    console.log("Invalid selection.");
-};
+// console.log("Turn number 5. ", gameState.playerName2, "'s turn.");
+// square = prompt("Player 1: Choose a square, 1-9.");
+// if (checkSquare(square-1)) {
+//     console.log("Valid square selection.");
+//     console.log(gameState.playerName1, " plays square number ", square);
+//     updateBoard(gameState.playerName1, square-1);
+//     console.log("Updated game board, turn number 6:");
+//     renderBoard();
+// } else {
+//     console.log("Invalid selection.");
+// };
 
-checkForWin();
+// checkForWin();
 
-console.log("Turn number 7. ", gameState.playerName1, "'s turn.");
-square = prompt("Player 1: Choose a square, 1-9.");
-if (checkSquare(square-1)) {
-    console.log("Valid square selection.");
-    console.log(gameState.playerName1, " plays square number ", square);
-    updateBoard(gameState.playerName1, square-1);
-    console.log("Updated game board, turn number 8:");
-    renderBoard();
-} else {
-    console.log("Invalid selection.");
-};
+// console.log("Turn number 6. ", gameState.playerName2, "'s turn.");
+// square = prompt("Player 2: Choose a square, 1-9.");
+// if (checkSquare(square-1)) {
+//     console.log("Valid square selection.");
+//     console.log(gameState.playerName2, " plays square number ", square);
+//     updateBoard(gameState.playerName2, square-1);
+//     console.log("Updated game board, turn number 7:");
+//     renderBoard();
+// } else {
+//     console.log("Invalid selection.");
+// };
 
-checkForWin();
+// checkForWin();
 
-console.log("Turn number 8. ", gameState.playerName2, "'s turn.");
-square = prompt("Player 2: Choose a square, 1-9.");
-if (checkSquare(square-1)) {
-    console.log("Valid square selection.");
-    console.log(gameState.playerName2, " plays square number ", square);
-    updateBoard(gameState.playerName2, square-1);
-    console.log("Updated game board, turn number 9:");
-    renderBoard();
-} else {
-    console.log("Invalid selection.");
-};
+// console.log("Turn number 7. ", gameState.playerName1, "'s turn.");
+// square = prompt("Player 1: Choose a square, 1-9.");
+// if (checkSquare(square-1)) {
+//     console.log("Valid square selection.");
+//     console.log(gameState.playerName1, " plays square number ", square);
+//     updateBoard(gameState.playerName1, square-1);
+//     console.log("Updated game board, turn number 8:");
+//     renderBoard();
+// } else {
+//     console.log("Invalid selection.");
+// };
 
-checkForWin();
+// checkForWin();
 
-console.log("Turn number 9. ", gameState.playerName1, "'s turn.");
-square = prompt("Player 1: Choose a square, 1-9.");
-if (checkSquare(square-1)) {
-    console.log("Valid square selection.");
-    console.log(gameState.playerName1, " plays square number ", square);
-    updateBoard(gameState.playerName1, square-1);
-    console.log("Updated game board, end of game.");
-    renderBoard();
-} else {
-    console.log("Invalid selection.");
-};
+// console.log("Turn number 8. ", gameState.playerName2, "'s turn.");
+// square = prompt("Player 2: Choose a square, 1-9.");
+// if (checkSquare(square-1)) {
+//     console.log("Valid square selection.");
+//     console.log(gameState.playerName2, " plays square number ", square);
+//     updateBoard(gameState.playerName2, square-1);
+//     console.log("Updated game board, turn number 9:");
+//     renderBoard();
+// } else {
+//     console.log("Invalid selection.");
+// };
 
-checkForWin();
-console.log("Game over with a tie.");
-// for (let i=1; i<= 9; i++) {
-//     if (i%2 !== 0) {
-//         console.log("Player 1's Turn.");
-//         let square = prompt(gameState.playerName1, ", choose a square, 0 - 9.");
-//         if (checkSquare(square)) {
-//             updateBoard(gameState.playerName1, square-1);
-//             renderBoard();
-//         // } else {
-//         //     square = prompt("Invalid selection. Please try again.");
-//         //     updateBoard(gameState.playerName1, square-1);
-//         //     renderBoard();
-//         // }
-//     } else {
-//         console.log("Player 2's Turn.");
-//         let square = prompt(gameState.playerName2, ", choose a square, 0 - 9.");
-//         if (checkSquare(square)) {
-//             updateBoard(gameState.playerName2, square-1);
-//             renderBoard();
-//         // } else {
-//         //     square = prompt("Invalid selection. Please try again.");
-//         //     updateBoard(gameState.playerName2, square-1);
-//         //     renderBoard();
-//         // }
+// checkForWin();
 
-//     }
-// //    updateBoard();
-// }
-//     }
-// }
-// console.log("Player 1 chooses square number 4.");
-// updateBoard(gameState.playerName1, 4);
-// console.log("Game Board Now: ", gameState.gameBoard);
-// renderBoard();
-// gameState.updateBoardState(gameState.playerName1, 3);
-// gameState.checkForWin();
+// console.log("Turn number 9. ", gameState.playerName1, "'s turn.");
+// square = prompt("Player 1: Choose a square, 1-9.");
+// if (checkSquare(square-1)) {
+//     console.log("Valid square selection.");
+//     console.log(gameState.playerName1, " plays square number ", square);
+//     updateBoard(gameState.playerName1, square-1);
+//     console.log("Updated game board, end of game.");
+//     renderBoard();
+// } else {
+//     console.log("Invalid selection.");
+// };
+
+// checkForWin();
+//console.log("Game over with a tie.");
